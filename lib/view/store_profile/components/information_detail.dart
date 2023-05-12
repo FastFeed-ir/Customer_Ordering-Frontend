bcc8f917a0b43bffa31740b35ca9a21cd7a05be1
Danial Tavakoli <dnialtavakoli@gmail.com>
2023-05-13 00:45:40 +0330

    feat(store-profile) : add information_detail.dart

---
+++ lib/view/store_profile/components/information_detail.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+Widget informationDetail(
+  IconData icon,
+  String title,
+  String subtitle,
+) {
+  return Container(
+    padding: const EdgeInsets.all(24),
+    color: Colors.white,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Row(
+          children: [
+            Icon(icon),
+            const SizedBox(width: 4.0),
+            Text(
+              '$title:',
+              style: const TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 24.0,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 8.0),
+        Text(
+          subtitle,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 16.0,
+          ),
+        ),
+        const SizedBox(height: 16.0),
+      ],
+    ),
+  );
+}
