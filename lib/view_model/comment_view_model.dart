91ab7c3fc48760b6f751714ac6379fae00c8c45a
Erfan Nasri <erfannasri2@gmail.com>
2023-05-15 12:03:57 +0330

    feat(CommentPage) : Add Comment ViewModel

---
+++ lib/view_model/comment_view_model.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+
+import 'package:customer_ordering_frontend/model/entity/comment.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../model/repository/comment_repository_impl.dart';
+
+class CmmentViewModel extends ChangeNotifier {
+  var repository = CommentRepositoryImpl();
+
+  StreamController<List<Comment>> collections =
+      StreamController<List<Comment>>();
+
+  void getComments(int storeId) async {
+    collections.add(await repository.getComments(storeId));
+    notifyListeners();
+  }
+
+  Future<int> addComment(Comment comment) async {
+    var newCollection = await repository.addComment(comment);
+    notifyListeners();
+    return newCollection.id ?? 0;
+  }
+}
