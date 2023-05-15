787eb506594bd18d5ad8c0d6f1541849cc439c9a
Erfan Nasri <erfannasri2@gmail.com>
2023-05-15 19:32:24 +0330

    fix(RatingPage) : Fix Order ViewModel StreamController name

--- lib/view_model/order_view_model.dart
+++ lib/view_model/order_view_model.dart
@@ -6,10 +6,10 @@ import '../model/repository/order_repository_impl.dart.dart';
 
 class OrderViewModel extends ChangeNotifier {
   var repository = OrderRepositoryImplDart();
-  StreamController<List<int>> collections = StreamController<List<int>>();
+  StreamController<List<int>> Ids = StreamController<List<int>>();
 
   void getProductsIds(int orderId) async {
-    collections.add(await repository.getOrdersProductsId(orderId));
+    Ids.add(await repository.getOrdersProductsId(orderId));
     notifyListeners();
   }
 }
