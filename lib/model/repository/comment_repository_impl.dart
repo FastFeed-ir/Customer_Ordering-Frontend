9633cd22c2adee5deb7aee93a8b6f606005dd2f4
Erfan Nasri <erfannasri2@gmail.com>
2023-05-15 12:03:38 +0330

    feat(CommentPage) : Add Comment Repos

---
+++ lib/model/repository/comment_repository_impl.dart
@@ -0,0 +1,35 @@
+import '../entity/comment.dart';
+import '../util/constants.dart';
+import 'comment_repository.dart';
+
+class CommentRepositoryImpl extends CommentRepository {
+  @override
+  Future<List<Comment>> getComments(int storeId) async {
+    var response = await dio.get('comments/?store_id=$storeId/');
+    print('response: ${response.statusMessage}');
+    if (response.data is List) {
+      List<dynamic> dataList = response.data;
+      List<Comment> comments = [];
+      for (var data in dataList) {
+        if (data is Map<String, dynamic>) {
+          var commentServer = Comment.fromJson(data);
+          comments.add(commentServer);
+        }
+      }
+      return comments;
+    } else {
+      throw Exception('Invalid response');
+    }
+  }
+
+  @override
+  Future<Comment> addComment(Comment comment) async {
+    var response = await dio.post(
+      'comments/',
+      data: comment,
+    );
+    print('response: ${response.statusMessage}');
+    final newCollection = Comment.fromJson(response.data);
+    return newCollection;
+  }
+}
