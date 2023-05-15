907f91a28a92a6953f91f79af0afbd81815e743b
Erfan Nasri <erfannasri2@gmail.com>
2023-05-15 16:46:34 +0330

    feat(RatingPage) : Add Rating ViewModel

---
+++ lib/view_model/rating_view_model.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import '../model/entity/rating.dart';
+import '../model/repository/rating_repository_impl.dart';
+
+class RatingViewModel extends ChangeNotifier {
+  var repository = RatingRepositoryImpl();
+
+  StreamController<List<Rating>> collections = StreamController<List<Rating>>();
+
+  void getRatings(int productId) async {
+    collections.add(await repository.getRatings(productId));
+    notifyListeners();
+  }
+
+  Future<int> addRating(Rating rating) async {
+    var newCollection = await repository.addComment(rating);
+    notifyListeners();
+    return newCollection.id ?? 0;
+  }
+}
