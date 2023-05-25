85f1a392c3abc5348f39ac365a491996da6f9b97
adel4780 <adelk138019@gmail.com>
2023-05-25 16:24:09 +0330

    feat(feature/Payment) : add order_repository.dart, order_repository_impl.dart

---
+++ lib/model/repository/order_repository.dart
@@ -0,0 +1,12 @@
+import '../entity/order.dart';
+
+abstract class OrderRepository {
+  Future<List<Order>> getOrders(int id);
+
+  Future<Order> addOrder(Order order);
+
+  Future<void> editOrder(Order order);
+
+  Future<void> deleteOrder(Order order);
+
+}
