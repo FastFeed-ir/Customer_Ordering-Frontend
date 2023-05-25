3c93c8c97c232e21552f6e653a8ab0141bc965f3
Erfan Nasri <erfannasri2@gmail.com>
2023-05-25 18:31:06 +0330

    feat(MainMenu) : Add getRatingsAndComments to  store viewModel

--- lib/view_model/store_viewmodel.dart
+++ lib/view_model/store_viewmodel.dart
@@ -1,29 +1,38 @@
 import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import '../model/entity/store.dart';
+import '../model/entity/storeRating.dart';
 import '../model/repository/store_repository_impl.dart';
 
 class StoreViewModel extends ChangeNotifier {
   var repository = StoreRepositoryImpl();
-
-  StreamController<List<Store>> stores =
-  StreamController<List<Store>>();
+  StoreRatingData storeRatingData = StoreRatingData();
+  StreamController<List<Store>> stores = StreamController<List<Store>>();
 
   void getStores(int id) async {
     stores.add(await repository.getStores(id));
     notifyListeners();
   }
+
   Future<Store> addStore(Store store) async {
     var newStore = await repository.addStore(store);
     notifyListeners();
     return newStore;
   }
+
   void editStore(Store store) async {
     repository.editStore(store);
     notifyListeners();
   }
+
   void deleteStore(Store store) async {
     repository.deleteStore(store);
+    notifyListeners();
+  }
+
+  void getRatingsAndComments(int storeId) async {
+    storeRatingData =
+        repository.getRatingsAndComments(storeId) as StoreRatingData;
     notifyListeners();
   }
 }
