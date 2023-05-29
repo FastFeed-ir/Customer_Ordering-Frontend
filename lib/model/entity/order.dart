f755de09d5e6c3f133071b1919eb546e17bdf82a
adel4780 <adelk138019@gmail.com>
2023-05-29 21:54:24 +0330

    fix(feature/Payment) : fix order model

--- lib/model/entity/order.dart
+++ lib/model/entity/order.dart
@@ -1,24 +1,24 @@
 class Order{
   int? id;
-  int? table_number;
+  int? tableNumber;
   String? description;
   int? store;
   Order({
     this.id,
-    required this.table_number,
+    required this.tableNumber,
     this.description,
     this.store
   });
   factory Order.fromJson(Map<String, dynamic> json) {
     return Order(
       id: json['id'],
-      table_number: json['table_number'],
+      tableNumber: json['table_number'],
       description: json['description'],
       store: json['store'],
     );
   }
   Map<String, dynamic> toJson() => {
-    'table_number' : table_number,
+    'table_number' : tableNumber,
     'title': description,
     'store': store,
   };
