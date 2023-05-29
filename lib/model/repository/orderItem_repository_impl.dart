171e3f06327699e258cba19dd6f21813a49c3293
adel4780 <adelk138019@gmail.com>
2023-05-29 21:57:05 +0330

    fix(feature/Payment) : fix orderItem_repository, orderItem_repository_impl.dart

--- lib/model/repository/orderItem_repository_impl.dart
+++ lib/model/repository/orderItem_repository_impl.dart
@@ -1,31 +1,12 @@
 import 'package:customer_ordering_frontend/model/entity/orderItem.dart';
 import 'package:dio/dio.dart';
+
 import '../util/constants.dart';
 import 'orderItem_repository.dart';
 
 class OrderItemRepositoryImpl extends OrderItemRepository {
   var dio = Dio(options);
 
-  @override
-  Future<List<OrderItem>> getOrderItems(int id) async {
-    var response = await dio.get('order_items/');
-    print('response: ${response.statusMessage}   responceCode: ${response.statusCode}');
-    if (response.data is List) {
-      List<dynamic> dataList = response.data;
-      List<OrderItem> orderItems = [];
-      for (var data in dataList) {
-        if (data is Map<String, dynamic>) {
-          var orderItem = OrderItem.fromJson(data);
-          if(orderItem.id == id) {
-            orderItems.add(orderItem);
-          }
-        }
-      }
-      return orderItems;
-    } else {
-      throw Exception('Invalid response');
-    }
-  }
   @override
   Future<OrderItem> addOrderItem(OrderItem orderItem) async {
     var response = await dio.post(
@@ -37,6 +18,15 @@ class OrderItemRepositoryImpl extends OrderItemRepository {
     final newOrderItem = OrderItem.fromJson(response.data);
     return newOrderItem;
   }
+
+  @override
+  Future<void> deleteOrderItem(OrderItem orderItem) async {
+    var response = await dio.delete(
+      'stores/${orderItem.id}/',
+    );
+    print('response: ${response.statusMessage}');
+  }
+
   @override
   Future<void> editOrderItem(OrderItem orderItem) async {
     var response = await dio.patch(
@@ -45,11 +35,24 @@ class OrderItemRepositoryImpl extends OrderItemRepository {
     );
     print('response: ${response.statusMessage}');
   }
+
   @override
-  Future<void> deleteOrderItem(OrderItem orderItem) async {
-    var response = await dio.delete(
-      'order_items/${orderItem.id}/',
-    );
-    print('response: ${response.statusMessage}');
+  Future<List<OrderItem>> getOrderItems(int orderId) async {
+    var response = await dio.get('order_items/?order_id=$orderId');
+    print(
+        'response: ${response.statusMessage}   responceCode: ${response.statusCode}');
+    if (response.data is List) {
+      List<dynamic> dataList = response.data;
+      List<OrderItem> orderItems = [];
+      for (var data in dataList) {
+        if (data is Map<String, dynamic>) {
+          var orderItem = OrderItem.fromJson(data);
+          orderItems.add(orderItem);
+        }
+      }
+      return orderItems;
+    } else {
+      throw Exception('Invalid response');
+    }
   }
 }
