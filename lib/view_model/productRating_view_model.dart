a69faa61f1a8f50ab5949f52ee22e2c72d94200c
adel4780 <adelk138019@gmail.com>
2023-05-29 22:15:21 +0330

    feat(feature/Payment) : add productRating_view_model.dart

---
+++ lib/view_model/productRating_view_model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import '../model/entity/productRating.dart';
+import '../model/repository/productRating_repository_impl.dart';
+
+class ProductRatingViewModel extends ChangeNotifier {
+  var repository = ProductRepositoryImpl();
+  ProductRatingData productRatingData = ProductRatingData();
+  void getRatings(int storeId) async {
+    productRatingData = repository.getRatings(storeId) as ProductRatingData;
+    notifyListeners();
+  }
+}
