f7c965e37805676a8a605603094ba7da81677815
adel4780 <adelk138019@gmail.com>
2023-05-30 11:52:41 +0330

    feat(feature/MainMenu) : add store name

--- lib/model/entity/storeRating.dart
+++ lib/model/entity/storeRating.dart
@@ -1,12 +1,14 @@
 class StoreRatingData {
+  String? name;
   int? commentCount;
   int? ratingCount;
   double? averageRating;
 
-  StoreRatingData({this.commentCount, this.ratingCount, this.averageRating});
+  StoreRatingData({this.name,this.commentCount, this.ratingCount, this.averageRating});
 
   factory StoreRatingData.fromJson(Map<String, dynamic> json) {
     return StoreRatingData(
+      name: json['name'],
       commentCount: json['comment_count'],
       ratingCount: json['rating_count'],
       averageRating: json['average_rating'],
