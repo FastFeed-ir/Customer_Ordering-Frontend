68ada849fc07ed62591103b98ba0de9006963ec6
Erfan Nasri <erfannasri2@gmail.com>
2023-05-30 05:39:57 +0330

    fix(develop) : fix main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:material_color_gen/material_color_gen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'utils/constants.dart';
 import 'view/ScanPage/pages/ScanPage.dart';
 import 'view/SelectTablePage/pages/SelectTablePage.dart';
-import 'view/lendingPage/pages/lending_page.dart';
+import 'view/lendingPage/pages/LendingPage.dart';
 
 void main() async {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late SharedPreferences prefs;
+  Future<void> x() async {
+    prefs = await SharedPreferences.getInstance();
+    prefs.setInt("StorId", 3);
+    prefs.setInt("tableCount", 8);
+  }
+
+  @override
+  initState() {
+    x();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +46,7 @@ class MyApp extends StatelessWidget {
             GetPage(name: LandingPage, page: () => LandingScreen()),
             //GetPage(name: MainMenuPage, page: () => MainMenuScreen()),
             //GetPage(name: PaymentPage, page: () => PaymentScreen()),
-            GetPage(name: SelectTablePage, page: () => SelectTableScreen()),
+            GetPage(name: SelectTablePage, page: () => SelectTableScreen(argument: Get.arguments)),
             //GetPage(name: SuccessfulPage, page: () => SuccessfulScreen()),
             //GetPage(name: InformationsPage, page: () => InformationsScreen()),
             //GetPage(name: CommentsPage, page: () => CommentsScreen(),),
@@ -36,7 +55,9 @@ class MyApp extends StatelessWidget {
             GetPage(name: ScanPage, page: () => const ScanScreen()),
           ],
           // title: 'FastFeed',
-          initialRoute: SendCommentPage,
+
+          initialRoute: LandingPage,
+          // initialRoute: ScanPage, //TODO
           textDirection: TextDirection.rtl,
           defaultTransition: Transition.noTransition,
           theme: ThemeData(
