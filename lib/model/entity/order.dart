aa3121079571633f3859e7d4a491d5df4064563d
Erfan Nasri <erfannasri2@gmail.com>
2023-06-02 14:56:25 +0330

    fix(develop) : connect Auth page to SelectTablePage

--- lib/model/entity/order.dart
+++ lib/model/entity/order.dart
@@ -1,13 +1,15 @@
-class Order{
+class Order {
   int? id;
   int? tableNumber;
   String? description;
   int? store;
+  int? authCode;
   Order({
     this.id,
     required this.tableNumber,
     this.description,
     required this.store,
+    this.authCode,
   });
   factory Order.fromJson(Map<String, dynamic> json) {
     return Order(
@@ -15,11 +17,12 @@ class Order{
       tableNumber: json['table_number'],
       description: json['description'],
       store: json['store'],
+      authCode: json['auth_code'],
     );
   }
   Map<String, dynamic> toJson() => {
-    'table_number' : tableNumber,
-    'description': description,
-    'store': store,
-  };
+        'table_number': tableNumber,
+        'description': description,
+        'store': store,
+      };
 }
