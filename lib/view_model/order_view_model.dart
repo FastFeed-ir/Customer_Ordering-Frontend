04ede4a3546c1187daabad8d890aec7870548dce
Erfan Nasri <erfannasri2@gmail.com>
2023-06-02 05:32:30 +0330

    fix(feature/RatingPage) : Add lastOrder from table viewModel

--- lib/view_model/order_view_model.dart
+++ lib/view_model/order_view_model.dart
@@ -7,27 +7,37 @@ import '../model/repository/order_repository_Impl.dart';
 class OrderViewModel extends ChangeNotifier {
   var repository = OrderRepositoryImpl();
 
-  StreamController<List<Order>> orders =
-  StreamController<List<Order>>();
+  StreamController<List<Order>> orders = StreamController<List<Order>>();
   StreamController<List<int>> Ids = StreamController<List<int>>();
-
+  late int lastOrderId;
+  late Order lastOrder;
   void getProductsIds(int orderId) async {
     Ids.add(await repository.getOrdersProductsId(orderId));
     notifyListeners();
   }
+
+  void getLastOrder(int storeId, int tableNumber) async {
+    lastOrder = repository.getLastOrderFromTable(storeId, tableNumber) as Order;
+    lastOrderId = lastOrder.id!;
+    notifyListeners();
+  }
+
   void getOrders(int id) async {
     orders.add(await repository.getOrders(id));
     notifyListeners();
   }
+
   Future<Order> addOrder(Order order) async {
     var newOrder = await repository.addOrder(order);
     notifyListeners();
     return newOrder;
   }
+
   void editOrder(Order order) async {
     repository.editOrder(order);
     notifyListeners();
   }
+
   void deleteOrder(Order order) async {
     repository.deleteOrder(order);
     notifyListeners();
