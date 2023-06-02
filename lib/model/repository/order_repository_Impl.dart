dafabf3deb8d648cfb360fb35c27ed111a11bb50
Erfan Nasri <erfannasri2@gmail.com>
2023-06-02 04:42:04 +0330

    fix(develop) : delete duplicate order repository file

--- lib/model/repository/order_repository_Impl.dart
+++ lib/model/repository/order_repository_Impl.dart
@@ -1,10 +1,22 @@
 import 'package:customer_ordering_frontend/model/entity/order.dart';
-import 'package:dio/dio.dart';
-import '../util/constants.dart';
 
+import '../util/constants.dart';
 import 'order_repository.dart';
-class OrderRepositoryImpl extends OrderRepository {
-  var dio = Dio(options);
+
+class OrderRepositoryImplDart extends OrderRepository {
+  @override
+  Future<List<int>> getOrdersProductsId(int orderId) async {
+    var response = await dio.get('orders/$orderId/productsID/');
+    print('response: ${response.statusMessage}');
+    if (response.statusCode == 200) {
+      final List<dynamic> productIds = response.data['product_ids'];
+      // Remove duplicates from the list
+      final List<dynamic> uniqueProductIds = productIds.toSet().toList();
+      return uniqueProductIds.cast<int>();
+    } else {
+      throw Exception('Invalid response');
+    }
+  }
 
   @override
   Future<List<Order>> getOrders(int id) async {
