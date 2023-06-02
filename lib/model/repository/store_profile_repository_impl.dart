0ada9aa3f308dcdac47a7333806809a9e13e3593
Erfan Nasri <erfannasri2@gmail.com>
2023-06-02 22:04:11 +0330

    feat(store-profile) : fix store_profile_repository_impl.dart

--- lib/model/repository/store_profile_repository_impl.dart
+++ lib/model/repository/store_profile_repository_impl.dart
@@ -17,7 +17,7 @@ class StoreProfileRepositoryImpl extends StoreProfileRepository {
         if (data is Map<String, dynamic>) {
           var commentServer = Comment.fromJson(data);
           //get just date from created at
-          commentServer.createdAt = commentServer.createdAt.split('T')[0];
+          commentServer.createdAt = commentServer.createdAt!.split('T')[0];
           comments.add(commentServer);
         }
       }
