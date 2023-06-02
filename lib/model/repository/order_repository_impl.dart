5ca2446f0ac87c87be71482ad2511701f29fd7b1
Erfan Nasri <erfannasri2@gmail.com>
2023-06-02 04:35:20 +0330

    fix(develop) : fix conflict in order_repository

--- lib/model/repository/order_repository_impl.dart
+++ lib/model/repository/order_repository_impl.dart
@@ -53,4 +53,18 @@ class OrderRepositoryImpl extends OrderRepository {
     );
     print('response: ${response.statusMessage}');
   }
+
+  @override
+  Future<List<int>> getOrdersProductsId(int orderId) async {
+    var response = await dio.get('orders/$orderId/productsID/');
+    print('response: ${response.statusMessage}');
+    if (response.statusCode == 200) {
+      final List<dynamic> productIds = response.data['product_ids'];
+      // Remove duplicates from the list
+      final List<dynamic> uniqueProductIds = productIds.toSet().toList();
+      return uniqueProductIds.cast<int>();
+    } else {
+      throw Exception('Invalid response');
+    }
+  }
 }
